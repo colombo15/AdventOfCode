0363fbd20b3feacfa1c4fb7c1d3e75453fa04aa3
Chris Gray <christophergray15@gmail.com>
2022-12-01 07:34:40 -0400

    2022 - Day 1

--- AdventOfCode/Puzzles/2022/Day01/Solution.cs
+++ AdventOfCode/Puzzles/2022/Day01/Solution.cs
@@ -4,29 +4,47 @@ namespace AdventOfCode.Puzzles._2022.Day01
 {
     internal sealed class Solution : ISolution
     {
+        private int _result;
+
         public void PartOne(string[] input)
         {
-            throw new NotImplementedException();
+            var elfCalories = new List<int>() { 0 };
+            foreach (var i in input)
+            {
+                if (i == "")
+                    elfCalories.Add(0);
+                else
+                    elfCalories[^1] += int.Parse(i);
+            }
+            _result = elfCalories.OrderByDescending(x => x).First();
         }
 
         public void PartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            var elfCalories = new List<int>() { 0 };
+            foreach (var i in input)
+            {
+                if (i == "")
+                    elfCalories.Add(0);
+                else
+                    elfCalories[^1] += int.Parse(i);
+            }
+            _result = elfCalories.OrderByDescending(x => x).Take(3).Sum();
         }
 
         public void Print()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(_result);
         }
 
         public bool IsPartOneCorrect()
         {
-            return false;
+            return _result == 68775;
         }
 
         public bool IsPartTwoCorrect()
         {
-            return false;
+            return _result == 202585;
         }
     }
 }
