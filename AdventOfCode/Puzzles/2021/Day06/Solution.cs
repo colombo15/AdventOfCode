cbc09bc54d7dbd3893a95af7ac881205cffd9479
Chris Gray <Chris.Gray@cvent.com>
2022-12-01 13:51:07 -0500

    2021 Day 6 and 7

---
+++ AdventOfCode/Puzzles/2021/Day06/Solution.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode.Common;
+using Microsoft.Diagnostics.Runtime.Utilities;
+
+namespace AdventOfCode.Puzzles._2021.Day06
+{
+    internal sealed class Solution : ISolution
+    {
+        private long _result;
+
+        public void PartOne(string[] input)
+        {
+            _result = CalculateLanternfish(input, 80);
+        }
+
+        public void PartTwo(string[] input)
+        {
+            _result = CalculateLanternfish(input, 256);
+        }
+
+        private static long CalculateLanternfish(string[] input, int days)
+        {
+            var fish = new long[9];
+
+            foreach (var item in input[0].Split(','))
+            {
+                fish[int.Parse(item)]++;
+            }
+
+            for (var iter = 0; iter < days; iter++)
+            {
+                var zero = fish[0];
+
+                for (var i = 0; i < fish.Length - 1; i++)
+                {
+                    fish[i] = fish[i + 1];
+                }
+
+                fish[8] = zero;
+                fish[6] += zero;
+            }
+
+            return fish.Sum();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_result);
+        }
+
+        public bool IsPartOneCorrect()
+        {
+            return _result == 391671;
+        }
+
+        public bool IsPartTwoCorrect()
+        {
+            return _result == 1754000560399;
+        }
+    }
+}
---
+++ AdventOfCode/Puzzles/2021/Day07/Solution.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode.Common;
+using System.Linq;
+
+namespace AdventOfCode.Puzzles._2021.Day07
+{
+    internal sealed class Solution : ISolution
+    {
+        private int _result;
+
+        public void PartOne(string[] input)
+        {
+            static int lambda(int target, int x) { return Math.Abs(target - x); }
+
+            var subs = input[0].Split(',').Select(x => int.Parse(x)).OrderBy(x => x).ToArray();
+            var median = subs[(int)Math.Floor((double)(subs.Length + 1) / 2)];
+            _result = CalculateFuel(subs, median, lambda);
+        }
+
+        public void PartTwo(string[] input)
+        {
+            static int lambda(int target, int x) { return Enumerable.Range(0, Math.Abs(target - x) + 1).Sum(); }
+
+            var subs = input[0].Split(',').Select(x => int.Parse(x)).OrderBy(x => x).ToArray();
+            var avg = subs.Average();
+            var calc1 = CalculateFuel(subs, (int)Math.Floor(avg), lambda);
+            var calc2 = CalculateFuel(subs, (int)Math.Ceiling(avg), lambda);
+            _result = calc1 < calc2 ? calc1: calc2;
+        }
+
+        private static int CalculateFuel(int[] subs, int target, Func<int, int, int> lambda)
+        {
+            return subs.Select(x => lambda(target, x)).Sum();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_result);
+        }
+
+        public bool IsPartOneCorrect()
+        {
+            return _result == 342534;
+        }
+
+        public bool IsPartTwoCorrect()
+        {
+            return _result == 94004208;
+        }
+    }
+}
