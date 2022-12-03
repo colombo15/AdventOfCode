34710a8afdee2e99060ae3b36a44f7a1e773169a
Chris Gray <christophergray15@gmail.com>
2022-12-03 09:15:31 -0400

    2022 Day 3

--- AdventOfCode/Puzzles/2022/Day03/Solution.cs
+++ AdventOfCode/Puzzles/2022/Day03/Solution.cs
@@ -4,29 +4,76 @@ namespace AdventOfCode.Puzzles._2022.Day03
 {
     internal sealed class Solution : ISolution
     {
+        private int _result;
+
         public void PartOne(string[] input)
         {
-            throw new NotImplementedException();
+            foreach (var str in input)
+            {
+                var itemTypes = new HashSet<char>();
+                var both = new HashSet<char>();
+
+                var subStrLength = str.Length / 2;
+
+                for (var i = 0; i < subStrLength; i++)
+                {
+                    itemTypes.Add(str[i]);
+                }
+
+                for (var i = subStrLength; i < str.Length; i++)
+                {
+                    if (itemTypes.Contains(str[i]) && both.Add(str[i]))
+                    {
+                        _result += char.IsLower(str[i]) ? str[i] - 96 : str[i] - 38;
+                    }
+                }
+            }
         }
 
         public void PartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < input.Length; i += 3)
+            {
+                var common1 = new HashSet<char>();
+                var common2 = new HashSet<char>();
+                var common3 = new HashSet<char>();
+
+                foreach (var c in input[i])
+                {
+                    common1.Add(c);
+                }
+
+                foreach (var c in input[i + 1])
+                {
+                    if (common1.Contains(c))
+                    {
+                        common2.Add(c);
+                    }
+                }
+
+                foreach (var c in input[i + 2])
+                {
+                    if (common2.Contains(c) && common3.Add(c))
+                    {
+                        _result += char.IsLower(c) ? c - 96 : c - 38;
+                    }
+                }
+            }
         }
 
         public void Print()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(_result);
         }
 
         public bool IsPartOneCorrect()
         {
-            return false;
+            return _result == 8185;
         }
 
         public bool IsPartTwoCorrect()
         {
-            return false;
+            return _result == 2817;
         }
     }
 }
