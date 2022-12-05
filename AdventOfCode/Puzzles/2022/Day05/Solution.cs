be716c27468f44f5821e121ef8a4c8ef275db3bf
Christopher Gray <christophergray15@gmail.com>
2022-12-05 09:00:55 -0400

    2022 day 5

--- AdventOfCode/Puzzles/2022/Day05/Solution.cs
+++ AdventOfCode/Puzzles/2022/Day05/Solution.cs
@@ -1,27 +1,115 @@
 ﻿using AdventOfCode.Common;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Puzzles._2022.Day05
 {
     internal sealed class Solution : ISolution
     {
+        public string _result;
+
         public void PartOne(string[] input)
         {
-            throw new NotImplementedException();
+            var regex = @"(?:(?:\[|\s)(.)(?:\]|\s)\s?)";
+            var stacks = new List<Stack<char>>();
+            var index = 0;
+
+            for (index = 0; index < input.Length; index++)
+            {
+                if (input[index].StartsWith(" 1")) break;
+
+                var match = Regex.Matches(input[index], regex);
+                
+                for (var i = 0; i < match.Count; i++)
+                {
+                    if (stacks.Count <= i) stacks.Add(new Stack<char>());
+                    if (match[i].Groups[1].Value == " ") continue;
+                    stacks[i].Push(match[i].Groups[1].Value[0]);
+                }
+            }
+
+            stacks = stacks.Select(stack => new Stack<char>(stack)).ToList();
+            index += 2;
+            regex = @"move (\d+) from (\d+) to (\d+)";
+
+            for (;index < input.Length; index++)
+            {
+                var match = Regex.Match(input[index], regex);
+                var x1 = int.Parse(match.Groups[1].Value);
+                var x2 = int.Parse(match.Groups[2].Value);
+                var x3 = int.Parse(match.Groups[3].Value);
+
+                while (x1 > 0)
+                {
+                    var p = stacks[x2 - 1].Pop();
+                    stacks[x3 - 1].Push(p);
+                    x1--;
+                }
+            }
+
+            foreach(var stack in stacks)
+            {
+                _result += stack.Peek();
+            }
         }
 
         public void PartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            var regex = @"(?:(?:\[|\s)(.)(?:\]|\s)\s?)";
+            var stacks = new List<Stack<char>>();
+            var index = 0;
+
+            for (index = 0; index < input.Length; index++)
+            {
+                if (input[index].StartsWith(" 1")) break;
+
+                var match = Regex.Matches(input[index], regex);
+
+                for (var i = 0; i < match.Count; i++)
+                {
+                    if (stacks.Count <= i) stacks.Add(new Stack<char>());
+                    if (match[i].Groups[1].Value == " ") continue;
+                    stacks[i].Push(match[i].Groups[1].Value[0]);
+                }
+            }
+
+            stacks = stacks.Select(stack => new Stack<char>(stack)).ToList();
+            index += 2;
+            regex = @"move (\d+) from (\d+) to (\d+)";
+
+            for (; index < input.Length; index++)
+            {
+                var match = Regex.Match(input[index], regex);
+                var x1 = int.Parse(match.Groups[1].Value);
+                var x2 = int.Parse(match.Groups[2].Value);
+                var x3 = int.Parse(match.Groups[3].Value);
+
+                var s = new Stack<char>();
+                while (x1 > 0)
+                {
+                    s.Push(stacks[x2 - 1].Pop());
+                    x1--;
+                }
+
+                while (s.Count > 0)
+                {
+                    stacks[x3 - 1].Push(s.Pop());
+                }
+            }
+
+            foreach (var stack in stacks)
+            {
+                _result += stack.Peek();
+            }
         }
 
         public void Print()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(_result);
         }
 
         public bool IsPartOneCorrect()
         {
-            return false;
+            return _result == "BSDMQFLSP";
         }
 
         public bool IsPartTwoCorrect()
