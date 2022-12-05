5dd9694ac3e9c44d11d5b82b7f4874fb511f6a27
Chris Gray <Chris.Gray@cvent.com>
2022-12-05 08:48:08 -0500

    GeneralizedSolution 2022 day 5

--- AdventOfCode/Puzzles/2022/Day05/Solution.cs
+++ AdventOfCode/Puzzles/2022/Day05/Solution.cs
@@ -9,45 +9,15 @@ namespace AdventOfCode.Puzzles._2022.Day05
 
         public void PartOne(string[] input)
         {
-            var stacks = new List<Stack<char>>();
-            var regex = @"(?:(?:\[|\s)(.)(?:\]|\s)\s?)";
-            int index;
-
-            for (index = 0; index < input.Length; index++)
-            {
-                if (input[index].StartsWith(" 1") || input[index] == " ") continue;
-                if (input[index].StartsWith("move")) break;
-
-                var match = Regex.Matches(input[index], regex);
-
-                for (var i = 0; i < match.Count; i++)
-                {
-                    if (stacks.Count <= i) stacks.Add(new Stack<char>());
-                    if (match[i].Groups[1].Value == " ") continue;
-                    stacks[i].Push(match[i].Groups[1].Value[0]);
-                }
-            }
-
-            stacks = stacks.Select(stack => new Stack<char>(stack)).ToList(); // Reverse stacks
-            regex = @"move (\d+) from (\d+) to (\d+)";
-
-            for (;index < input.Length; index++)
-            {
-                var match = Regex.Match(input[index], regex);
-                var count = int.Parse(match.Groups[1].Value);
-                var i1 = int.Parse(match.Groups[2].Value) - 1;
-                var i2 = int.Parse(match.Groups[3].Value) - 1;
-
-                while (count-- > 0) stacks[i2].Push(stacks[i1].Pop());
-            }
-
-            foreach(var stack in stacks)
-            {
-                _result += stack.Peek();
-            }
+            GeneralizedSolution(input, 9000);
         }
 
         public void PartTwo(string[] input)
+        {
+            GeneralizedSolution(input, 9001);
+        }
+
+        private void GeneralizedSolution(string[] input, int crateMoverModel)
         {
             var stacks = new List<Stack<char>>();
             var regex = @"(?:(?:\[|\s)(.)(?:\]|\s)\s?)";
@@ -78,9 +48,16 @@ namespace AdventOfCode.Puzzles._2022.Day05
                 var count = int.Parse(match.Groups[1].Value);
                 var i1 = int.Parse(match.Groups[2].Value) - 1;
                 var i2 = int.Parse(match.Groups[3].Value) - 1;
-                
-                while (count-- > 0) tempStack.Push(stacks[i1].Pop());
-                while (tempStack.Count > 0) stacks[i2].Push(tempStack.Pop());
+
+                if (crateMoverModel == 9000)
+                {
+                    while (count-- > 0) stacks[i2].Push(stacks[i1].Pop());
+                }
+                else if (crateMoverModel == 9001)
+                {
+                    while (count-- > 0) tempStack.Push(stacks[i1].Pop());
+                    while (tempStack.Count > 0) stacks[i2].Push(tempStack.Pop());
+                }
             }
 
             foreach (var stack in stacks)
