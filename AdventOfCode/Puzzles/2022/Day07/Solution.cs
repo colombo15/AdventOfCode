3a6bb7decdefb171d6c8f16c28744a25e98bf424
Christopher Gray <christophergray15@gmail.com>
2022-12-07 08:22:00 -0400

    2022 day 7

--- AdventOfCode/Puzzles/2022/Day07/Solution.cs
+++ AdventOfCode/Puzzles/2022/Day07/Solution.cs
@@ -4,19 +4,107 @@ namespace AdventOfCode.Puzzles._2022.Day07
 {
     internal sealed class Solution : ISolution
     {
+        private long _result;
+
         public void PartOne(string[] input)
         {
-            throw new NotImplementedException();
+            var root = new TreeNode();
+            var nodeReferences = new List<TreeNode>() { root };
+            var curr = root;
+
+            foreach (var item in input)
+            {
+                if (item == "$ cd /") continue;
+
+                if (item.StartsWith("$ cd"))
+                {
+                    if (item.EndsWith(".."))
+                    {
+                        curr = curr.Parent;
+                    }
+                    else
+                    {
+                        var newNode = new TreeNode();
+                        nodeReferences.Add(newNode);
+                        newNode.Parent = curr;
+                        curr.Children.Add(newNode);
+                        curr = newNode;
+                    }
+                }
+                else if (!item.StartsWith("$ ls") && !item.StartsWith("dir"))
+                {
+                    curr.FileSize += long.Parse(item.Split(' ').First());
+                }
+            }
+
+            _result = nodeReferences.Select((x) => x.GetTotalFileSize()).Where((x) => x <= 100000).Sum();
         }
 
         public void PartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            var root = new TreeNode();
+            var nodeReferences = new List<TreeNode>() { root };
+            var curr = root;
+
+            foreach (var item in input)
+            {
+                if (item == "$ cd /") continue;
+
+                if (item.StartsWith("$ cd"))
+                {
+                    if (item.EndsWith(".."))
+                    {
+                        curr = curr.Parent;
+                    }
+                    else
+                    {
+                        var newNode = new TreeNode();
+                        nodeReferences.Add(newNode);
+                        newNode.Parent = curr;
+                        curr.Children.Add(newNode);
+                        curr = newNode;
+                    }
+                }
+                else if (!item.StartsWith("$ ls") && !item.StartsWith("dir"))
+                {
+                    curr.FileSize += long.Parse(item.Split(' ').First());
+                }
+            }
+
+            var fileSizes = nodeReferences.Select((x) => x.GetTotalFileSize()).OrderBy((x) => x).ToArray();
+
+            var unusedSpace = 70000000 - fileSizes.Last();
+            for (var i = 0; i < fileSizes.Length; i++)
+            {
+                if (unusedSpace + fileSizes[i] >= 30000000)
+                {
+                    _result = fileSizes[i];
+                    return;
+                }
+            }
+        }
+
+        private class TreeNode
+        {
+            public List<TreeNode> Children = new List<TreeNode>();
+            public long FileSize = 0;
+            public TreeNode Parent;
+            public long GetTotalFileSize()
+            {
+                var result = FileSize;
+
+                foreach (var c in Children)
+                {
+                    result += c.GetTotalFileSize();
+                }
+
+                return result;
+            }
         }
 
         public void Print()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(_result);
         }
 
         public bool IsPartOneCorrect()
