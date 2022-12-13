af3777ec11848f574f69a551d73b5538473c834d
Christopher Gray <christophergray15@gmail.com>
2022-12-13 08:37:25 -0400

    init of 2022 day 13

--- AdventOfCode/Puzzles/2022/Day13/Solution.cs
+++ AdventOfCode/Puzzles/2022/Day13/Solution.cs
@@ -1,12 +1,28 @@
 ﻿using AdventOfCode.Common;
+using System.Text.RegularExpressions;
+using static BenchmarkDotNet.Engines.Engine;
 
 namespace AdventOfCode.Puzzles._2022.Day13
 {
-    internal sealed class Solution : ISolution
+    internal sealed partial class Solution : ISolution
     {
+        private int _result;
+
         public void PartOne(string[] input)
         {
-            throw new NotImplementedException();
+            var signals = new List<Signal>();
+
+            for (var i = 0; i < input.Length; i += 3)
+            {
+                signals.Add(new Signal(input[i], input[i + 1]));
+            }
+
+            for (var i = 0; i < signals.Count; i++)
+            {
+                if (signals[i].IsRightOrder())
+                    _result += i + 1;
+            }
+
         }
 
         public void PartTwo(string[] input)
@@ -14,19 +30,44 @@ namespace AdventOfCode.Puzzles._2022.Day13
             throw new NotImplementedException();
         }
 
-        public void Print()
+        private partial class Signal
         {
-            throw new NotImplementedException();
-        }
+            public List<int> Signal1 { get; private set; }
+            public List<int> Signal2 { get; private set; }
 
-        public bool IsPartOneCorrect()
-        {
-            return false;
-        }
+            public Signal(string s1, string s2) 
+            {
+                Signal1 = new List<int>();
+                Signal2 = new List<int>();
 
-        public bool IsPartTwoCorrect()
-        {
-            return false;
+                var match = MyRegex().Matches(s1);
+                Signal1 = new List<int>(match.Select(x => int.Parse(x.Value)));
+
+                match = MyRegex().Matches(s2);
+                Signal2 = new List<int>(match.Select(x => int.Parse(x.Value)));
+            }
+
+            public bool IsRightOrder()
+            {
+                for (var i = 0; i < Signal1.Count; i++)
+                {
+                    if (Signal2.Count <= i)
+                    {
+                        return false;
+                    }
+
+                    if (Signal1[i] > Signal2[i])
+                    {
+                        return false;
+
+                    }
+                }
+
+                return true;
+            }
+
+            [GeneratedRegex("\\d+")]
+            private static partial Regex MyRegex();
         }
     }
 }
---
+++ AdventOfCode/Puzzles/2022/Day13/SolutionPartial.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Puzzles._2022.Day13
+{
+    internal sealed partial class Solution : ISolution
+    {
+        public void Print()
+        {
+            Console.WriteLine(_result);
+        }
+
+        public bool IsPartOneCorrect()
+        {
+            return false;
+        }
+
+        public bool IsPartTwoCorrect()
+        {
+            return false;
+        }
+    }
+}
