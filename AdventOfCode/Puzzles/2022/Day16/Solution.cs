8bf71339db9153487b7205410e20a2e040b7a580
Christopher Gray <christophergray15@gmail.com>
2022-12-16 08:23:41 -0400

    2022 day16 init

--- AdventOfCode/Puzzles/2022/Day16/Solution.cs
+++ AdventOfCode/Puzzles/2022/Day16/Solution.cs
@@ -1,11 +1,19 @@
 ﻿using AdventOfCode.Common;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Puzzles._2022.Day16
 {
-    internal sealed class Solution : ISolution
+    internal sealed partial class Solution : ISolution
     {
+        private int _result;
+
         public void PartOne(string[] input)
         {
+            var rooms = ParseInput(input);
+            var time = 30;
+
+            _result = FindHighestPreasure(rooms["AA"], time, 0);
+
             throw new NotImplementedException();
         }
 
@@ -14,19 +22,82 @@ namespace AdventOfCode.Puzzles._2022.Day16
             throw new NotImplementedException();
         }
 
-        public void Print()
+        private int FindHighestPreasure(Vertex room, int time, int currflow)
         {
-            throw new NotImplementedException();
+            if (time <= 0)
+                return currflow;
+
+            if (room.Flow > 0) 
+            {
+                currflow += room.Flow * time;
+                room.Flow = 0;
+                time -= 1;
+            }
+
+            var highest = 0;
+            foreach (var child in room.Vertices)
+            {
+                var retval = FindHighestPreasure(child, time - 1, currflow);
+                if (retval > highest)
+                    highest = retval;
+            }
+
+            return highest;
         }
 
-        public bool IsPartOneCorrect()
+        private Dictionary<string, Vertex> ParseInput(string[] input)
         {
-            return false;
+            var rooms = new Dictionary<string, Vertex>();
+            foreach (var item in input)
+            {
+                var match = MyRegex().Match(item);
+
+                var roomName = match.Groups[1].Value;
+                var flowRate = int.Parse(match.Groups[2].Value);
+                var connectedRooms = match.Groups[3].Value.Split(", ");
+                var room = new Vertex(roomName, flowRate);
+
+                if (rooms.ContainsKey(roomName))
+                {
+                    rooms[roomName].Flow = flowRate;
+                    room = rooms[roomName];
+                }
+                else
+                {
+                    rooms.Add(roomName, room);
+                }
+
+                foreach (var r in connectedRooms)
+                {
+                    if (!rooms.ContainsKey(r))
+                        rooms.Add(r, new Vertex(r));
+                    room.AddChildren(rooms[r]);
+                }
+
+            }
+            return rooms;
         }
 
-        public bool IsPartTwoCorrect()
+        [GeneratedRegex("Valve (.+) has flow rate=(\\d+); tunnels? leads? to valves? (.+)(,\\s\\.+)*")]
+        private static partial Regex MyRegex();
+
+        private sealed record class Vertex
         {
-            return false;
+            public string Name { get; set; }
+            public int Flow { get; set; }
+            public List<Vertex> Vertices { get; set; }
+
+            public Vertex(string name, int flow = 0) 
+            { 
+                Vertices = new List<Vertex>();
+                Name = name;
+                Flow = flow;
+            }
+
+            public void AddChildren(Vertex child)
+            {
+                Vertices.Add(child);
+            }
         }
     }
 }
---
+++ AdventOfCode/Puzzles/2022/Day16/SolutionPartial.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Common;
+using Microsoft.Diagnostics.Runtime.Utilities;
+
+namespace AdventOfCode.Puzzles._2022.Day16
+{
+    internal sealed partial class Solution : ISolution
+    {
+        public void Print()
+        {
+            Console.WriteLine(_result);
+        }
+
+        public bool IsPartOneCorrect()
+        {
+            return _result == 5716881;
+        }
+
+        public bool IsPartTwoCorrect()
+        {
+            return _result == 10852583132904;
+        }
+    }
+}
