f522aafd0bad1b3c93c3097fe47b38b59b00eff8
Christopher Gray <christophergray15@gmail.com>
2022-12-18 09:33:48 -0400

    2022 day 18

--- AdventOfCode/Puzzles/2022/Day18/Solution.cs
+++ AdventOfCode/Puzzles/2022/Day18/Solution.cs
@@ -2,31 +2,136 @@
 
 namespace AdventOfCode.Puzzles._2022.Day18
 {
-    internal sealed class Solution : ISolution
+    internal sealed partial class Solution : ISolution
     {
+        private int _result;
+        private HashSet<Coord> _coords = new();
+        private int _xWall = 0;
+        private int _yWall = 0;
+        private int _zWall = 0;
+
         public void PartOne(string[] input)
         {
-            throw new NotImplementedException();
+            ParseInput(input);
+
+            foreach (var c in _coords)
+            {
+                if (!_coords.Contains(c with { X = c.X + 1 })) _result++;
+                if (!_coords.Contains(c with { X = c.X - 1 })) _result++;
+                if (!_coords.Contains(c with { Y = c.Y + 1 })) _result++;
+                if (!_coords.Contains(c with { Y = c.Y - 1 })) _result++;
+                if (!_coords.Contains(c with { Z = c.Z + 1 })) _result++;
+                if (!_coords.Contains(c with { Z = c.Z - 1 })) _result++;
+            }
         }
 
         public void PartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            ParseInput(input);
+
+            _xWall = _coords.Select(c => c.X).Max() + 5;
+            _yWall = _coords.Select(c => c.Y).Max() + 5;
+            _zWall = _coords.Select(c => c.Z).Max() + 5;
+
+            foreach (var c in _coords)
+            {
+                if (IsExposed(c with { X = c.X + 1 })) _result++;
+                if (IsExposed(c with { X = c.X - 1 })) _result++;
+                if (IsExposed(c with { Y = c.Y + 1 })) _result++;
+                if (IsExposed(c with { Y = c.Y - 1 })) _result++;
+                if (IsExposed(c with { Z = c.Z + 1 })) _result++;
+                if (IsExposed(c with { Z = c.Z - 1 })) _result++;
+            }
         }
 
-        public void Print()
+        private bool IsExposed(Coord c)
         {
-            throw new NotImplementedException();
-        }
+            if (_coords.Contains(c)) return false;
 
-        public bool IsPartOneCorrect()
-        {
+            var visited = new HashSet<Coord>();
+            var check = new Queue<Coord>();
+
+            check.Enqueue(c with { X = c.X + 1 });
+            check.Enqueue(c with { X = c.X - 1 });
+            check.Enqueue(c with { Y = c.Y + 1 });
+            check.Enqueue(c with { Y = c.Y - 1 });
+            check.Enqueue(c with { Z = c.Z + 1 });
+            check.Enqueue(c with { Z = c.Z - 1 });
+
+            while(check.Any())
+            {
+                var temp = check.Dequeue();
+                visited.Add(temp);
+
+                if (_coords.Contains(temp)) continue;
+
+                var next = temp with { X = temp.X + 1 };
+                if (!visited.Contains(next) && !_coords.Contains(next) && !check.Contains(next))
+                {
+                    if (next.X >= _xWall) return true;
+                    check.Enqueue(next);
+                }
+
+                next = temp with { X = temp.X - 1 };
+                if (!visited.Contains(next) && !_coords.Contains(next) && !check.Contains(next))
+                {
+                    if (next.X <= 0) return true;
+                    check.Enqueue(next);
+                }
+
+                next = temp with { Y = temp.Y + 1 };
+                if (!visited.Contains(next) && !_coords.Contains(next) && !check.Contains(next))
+                {
+                    if (next.Y >= _yWall) return true;
+                    check.Enqueue(next);
+                }
+
+                next = temp with { Y = temp.Y - 1 };
+                if (!visited.Contains(next) && !_coords.Contains(next) && !check.Contains(next))
+                {
+                    if (next.Y <= 0) return true;
+                    check.Enqueue(next);
+                }
+
+                next = temp with { Z = temp.Z + 1 };
+                if (!visited.Contains(next) && !_coords.Contains(next) && !check.Contains(next))
+                {
+                    if (next.Z >= _zWall) return true;
+                    check.Enqueue(next);
+                }
+
+                next = temp with { Z = temp.Z - 1 };
+                if (!visited.Contains(next) && !_coords.Contains(next) && !check.Contains(next))
+                {
+                    if (next.Z <= 0) return true;
+                    check.Enqueue(next);
+                }
+            }
+
             return false;
         }
 
-        public bool IsPartTwoCorrect()
+        private void ParseInput(string[] input)
         {
-            return false;
+            foreach (var item in input)
+            {
+                _coords.Add(new Coord(item));
+            }
+        }
+
+        private record class Coord
+        {
+            public int X; 
+            public int Y;
+            public int Z;
+
+            public Coord(string input)
+            {
+                var split = input.Split(',');
+                this.X = int.Parse(split[0]);
+                this.Y = int.Parse(split[1]);
+                this.Z = int.Parse(split[2]);
+            }
         }
     }
 }
---
+++ AdventOfCode/Puzzles/2022/Day18/SolutionPartial.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Common;
+using Microsoft.Diagnostics.Runtime.Utilities;
+
+namespace AdventOfCode.Puzzles._2022.Day18
+{
+    internal sealed partial class Solution : ISolution
+    {
+        public void Print()
+        {
+            Console.WriteLine(_result);
+        }
+
+        public bool IsPartOneCorrect()
+        {
+            return _result == 4320;
+        }
+
+        public bool IsPartTwoCorrect()
+        {
+            return _result == 1751;
+        }
+    }
+}
