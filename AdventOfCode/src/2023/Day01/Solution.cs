b9e12f193d9a1c7634430e8fd07d203ec60091a7
Gray, Chris <Chris.Gray@cvent.com>
2023-12-01 11:22:02 -0400

    2023 day 1 part 2 refactor

--- AdventOfCode/src/2023/Day01/Solution.cs
+++ AdventOfCode/src/2023/Day01/Solution.cs
@@ -16,13 +16,15 @@ public partial class Solution : ISolution
 
 	public void Puzzle2(string[] input)
 	{
-		var total = (
-			from i in input 
-			let first = Part2Regex_FromFront().Matches(i).First().Value 
-			let last = Part2Regex_FromBack().Matches(i).First().Value 
-			select int.Parse(Convert(first) + Convert(last))
-			).Sum();
-
+		var total = input
+			.Select(i =>
+			{
+				var first = Part2Regex_FromFront().Matches(i).First().Value;
+				var last = Part2Regex_FromBack().Matches(i).First().Value;
+				return int.Parse(Convert(first) + Convert(last));
+			})
+			.Sum();
+		
 		Console.WriteLine(total);
 	}
 
