edfa9ba7957dbcf52052055494b30201150abe79
Christopher Gray <christophergray15@gmail.com>
2023-12-01 11:00:25 -0400

    2023 day 1 with regex

--- AdventOfCode/src/2023/Day01/Solution.cs
+++ AdventOfCode/src/2023/Day01/Solution.cs
@@ -1,35 +1,18 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode._2023.Day01;
 
-public class Solution : ISolution
+public partial class Solution : ISolution
 {
 	public void Puzzle1(string[] input)
 	{
 		var total = 0;
 
-		foreach (var item in input)
+		foreach (var i in input)
 		{
-			var number = "";
-			for (var i = 0; i < item.Length; i++)
-			{
-				if (int.TryParse(item[i].ToString(), out var num))
-				{
-					number += num;
-					break;
-				}
-			}
-
-            for (var i = item.Length - 1; i >= 0; i--)
-            {
-                if (int.TryParse(item[i].ToString(), out var num))
-                {
-                    number += num;
-                    break;
-                }
-            }
-
-			total += int.Parse(number);
+            var matches = Part1Regex().Matches(i);
+            total += int.Parse(matches.First().Value + matches.Last().Value);
         }
-
 
 		Console.WriteLine(total);
 	}
@@ -38,88 +21,40 @@ public class Solution : ISolution
 	{
         var total = 0;
 
-        foreach (var item in input)
+        foreach (var i in input)
         {
-            var number = "";
-            for (var i = 0; i < item.Length; i++)
-            {
-                var num = GetNum(item, i);
-                if (num != -1)
-                {
-                    number += num;
-                    break;
-                }
-            }
 
-            for (var i = item.Length - 1; i >= 0; i--)
-            {
-                var num = GetNum(item, i);
-                if (num != -1)
-                {
-                    number += num;
-                    break;
-                }
-            }
-
-            total += int.Parse(number);
+            var first = Part2Regex_FromFront().Matches(i).First().Value;
+            var last = Part2Regex_FromBack().Matches(i).First().Value;
+            total += int.Parse(Convert(first) + Convert(last));
         }
-
 
         Console.WriteLine(total);
+    }
 
-        int GetNum(string item, int i)
+    [GeneratedRegex("[0-9]")]
+    private static partial Regex Part1Regex();
+
+    [GeneratedRegex("([0-9]|one|two|three|four|five|six|seven|eight|nine)")]
+    private static partial Regex Part2Regex_FromFront();
+
+    [GeneratedRegex("([0-9]|one|two|three|four|five|six|seven|eight|nine)", RegexOptions.RightToLeft)]
+    private static partial Regex Part2Regex_FromBack();
+
+    private static string Convert(string s)
+    {
+        return s switch
         {
-            if (int.TryParse(item[i].ToString(), out var num))
-            {
-                return num;
-            }
-
-            if (i <= item.Length - 3 && item.Substring(i, 3) == "one")
-            {
-                return 1;
-            }
-
-            if (i <= item.Length - 3 && item.Substring(i, 3) == "two")
-            {
-                return 2;
-            }
-
-            if (i <= item.Length - 5 && item.Substring(i, 5) == "three")
-            {
-                return 3;
-            }
-
-            if (i <= item.Length - 4 && item.Substring(i, 4) == "four")
-            {
-                return 4;
-            }
-
-            if (i <= item.Length - 4 && item.Substring(i, 4) == "five")
-            {
-                return 5;
-            }
-
-            if (i <= item.Length - 3 && item.Substring(i, 3) == "six")
-            {
-                return 6;
-            }
-
-            if (i <= item.Length - 5 && item.Substring(i, 5) == "seven")
-            {
-                return 7;
-            }
-
-            if (i <= item.Length - 5 && item.Substring(i, 5) == "eight")
-            {
-                return 8;
-            }
-
-            if (i <= item.Length - 4 && item.Substring(i, 4) == "nine")
-            {
-                return 9;
-            }
-
-            return -1;
-        }
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => s
+        };
     }
 }
